a46a62b6ff2ade741b649b57db00a96f299ae595
Volodymyr-Bodak <122619914+Volodymyr-Bodak@users.noreply.github.com>
2023-05-27 11:00:57 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -9,11 +9,9 @@ if (JSON.parse(localStorage.getItem("feedback-form-state"))) {
 
 form.addEventListener('input', throttle(infoKeep, 500));
 
+
 function infoKeep(event) {
-  const {
-    email,
-    message
-  } = event.currentTarget.elements;
+  const { email, message } = form.elements;
 
   const data = {
     email: email.value,
@@ -22,16 +20,18 @@ function infoKeep(event) {
 
   localStorage.setItem("feedback-form-state", JSON.stringify(data));
 
-  console.log(data);
-  console.log(JSON.parse(localStorage.getItem("feedback-form-state")).email)
+  
+  
   
 
 }
 
 form.addEventListener('submit', function(event) {
   event.preventDefault();
-
+  
   localStorage.clear();
+  const formData = JSON.parse(localStorage.getItem('feedback-form-state'));
+  console.log(formData);
 
   form.reset();
 });
